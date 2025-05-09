a564488c28d83d8df8f2c9ff64f7dc3aa111d36d
Steve Cassidy <steve.cassidy@mq.edu.au>
2025-05-09 11:27:33 +1000

    tweaks
    Signed-off-by: Steve Cassidy <steve.cassidy@mq.edu.au>

--- lib/screens/auth_screen/auth_screen.dart
+++ lib/screens/auth_screen/auth_screen.dart
@@ -12,25 +12,28 @@ class AuthScreen extends StatelessWidget {
     return Consumer<StateModel>(
       
       builder: (context, state, child) {
-        return Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Image.asset(
-                'assets/images/HP_Quiz_Logo.jpg',
-                width: 300,
-              ),
-              const SizedBox(height: 30),
-              const Text(
-                "COMP3130 Quiz App",
-                style: TextStyle(
-                  color: Color(0xff401201),
-                  fontSize: 22,
-                ),
-              ),
-              const SizedBox(height: 30),
+        return Container(
+              color: const Color(0xffF2D3AC),
+              child: Center(
+                child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Image.asset(
+                    'assets/images/HP_Quiz_Logo.jpg',
+                    width: 300,
+                  ),
+                  const SizedBox(height: 30),
+                  const Text(
+                    "COMP3130 Quiz App",
+                    style: TextStyle(
+                      color: Color(0xff401201),
+                      fontSize: 22,
+                    ),
+                  ),
+                  const SizedBox(height: 30),
                   const AuthWidget()
-            ],
+                ],
+            ),
           ),
         );
       });
